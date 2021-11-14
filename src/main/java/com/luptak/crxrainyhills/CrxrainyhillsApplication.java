a5b039500d506ebdc1051137160decaeafb2b136
Martin Luptak <martinxluptak@gmail.com>
2021-11-14 12:00:07 +0100

    Add empty Spring Initializr IntelliJ IDEA Project
    The enabled plugins are:
    *  Spring Web
    *  Spring DevTools
    
    Signed-off-by: Martin Luptak <martinxluptak@gmail.com>

---
+++ src/main/java/com/luptak/crxrainyhills/CrxrainyhillsApplication.java
@@ -0,0 +1,13 @@
+package com.luptak.crxrainyhills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrxrainyhillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrxrainyhillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luptak/crxrainyhills/CrxrainyhillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luptak.crxrainyhills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrxrainyhillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

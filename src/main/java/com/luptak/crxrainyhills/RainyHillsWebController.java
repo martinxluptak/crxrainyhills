cae07ce1775082c99071890de3b2057b66f29eb8
Martin Luptak <martinxluptak@gmail.com>
2021-11-17 15:37:40 +0100

    Attach business logic to RainyHillsWebController
    Signed-off-by: Martin Luptak <martinxluptak@gmail.com>

--- src/main/java/com/luptak/crxrainyhills/RainyHillsWebController.java
+++ src/main/java/com/luptak/crxrainyhills/RainyHillsWebController.java
@@ -5,22 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 /**
  * Handles web queries for CrxrainyhillsApplication.
  */
 @RestController
 public class RainyHillsWebController {
 
+    RainCalculator rainCalculator = new RainCalculatorImpl();
+
     /**
-     * TODO: Attach business logic.
-     *
      * @param surfaces An array of integers describing the height profile of a surface.
      * @return an integer specifying the maximum rain water volume of this height profile.
      */
     @GetMapping("/rainy")
-    public String getMaxRainWaterVolume(@RequestParam int[] surfaces) {
-        return "Hello world! Input: " + Arrays.toString(surfaces);
+    public int getMaxRainWaterVolume(@RequestParam int[] surfaces) {
+        return rainCalculator.getMaxWater(surfaces);
     }
 }
